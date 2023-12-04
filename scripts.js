f4936723af6ee2af44c0c35a016cb7d3a66302ff
Vicente Calderon <jvicentecv@gmail.com>
2023-12-04 12:48:03 -0400

    crear insertar elementos

--- scripts.js
+++ scripts.js
@@ -393,25 +393,66 @@ const button = document.getElementById('button')
 //     console.log(e)
 // })
 
-const galery = document.getElementById('galery')
-const link = document.getElementById('link')
+// const galery = document.getElementById('galery')
+// const link = document.getElementById('link')
 
-// galery.addEventListener('click',(e) =>{
-//     e.target.classList.add('red')
+// // galery.addEventListener('click',(e) =>{
+// //     e.target.classList.add('red')
+// // })
+// link.addEventListener('click', (e) =>{
+//     e.preventDefault()
+//     button.click()
 // })
-link.addEventListener('click', (e) =>{
-    e.preventDefault()
-    button.click()
-})
-form.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    console.log('el formulario se a enviado')
-})
+// form.addEventListener('submit', (e) =>{
+//     e.preventDefault()
+//     console.log('el formulario se a enviado')
+// })
 
 
-button.addEventListener('click', (e) =>{
-    input.value='haz echoi click'
-})
+// button.addEventListener('click', (e) =>{
+//     input.value='haz echoi click'
+// })
+
+//CREAR E INSERTAR ELEMENTOS
+/**
+ * Crear un elemento: document.createElement(element) 
+ * Escribir texto en un elemento: element.textContent texto 
+ * Escribir HTML en un elemento: element.innerHTML codigo HTML 
+ * 
+ * AÑADIR UN ELEMENTO AL DOM :parent.appendChild(element) 
+
+Fragmentos de codigo: document.createDocumentFragment()   */
+
+const days = ['Lunes', 'Mertes', 'Miercoles., Thieves', 'Viernes', 'Sabado', 'Domingo'] 
+
+const title = document.getElementById('title')
+const daysList = document.getElementById('daysList')
+const selectDays = document.getElementById('daysSelect') 
+
+//const itemList = document.createElement('LI')
+
+//daysList.appendChild(itemList)
+
+title.innerHTML = 'dom  <span>crear e insertar elementos I</span> '
+
+//itemList.textContent ='Lunes'
+//console.log(daysList)
+
+const fragment= document.createDocumentFragment()
+// for(const day of days){
+//     const itemList = document.createElement('LI')
+//   itemList.textContent =day
+//    fragment.appendChild(itemList)
+// }
+
+for(const day of days){
+    const selectItem= document.createElement('OPTION')
+    selectItem.setAttribute('value',day.toLowerCase())
+    selectItem.textContent=day
+    fragment.appendChild(selectItem)
+}
+selectDays.appendChild(fragment)
+
 
 
 
